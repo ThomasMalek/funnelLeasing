e430c49905e785c1a2c5930b0a0726d58dfbc7e8
Thomas <thomas.mccarthy@classlink.com>
2022-08-01 02:27:00 -0400

    Fixed logic error in unit tests

--- satellite.test.js
+++ satellite.test.js
@@ -4,18 +4,21 @@ import {jest} from '@jest/globals'
 const testSatellite = new Satellite();
 
 test("160 Altitude Returns A-OK Status",()=>{
+    testSatellite.emergencyCountDown = 60;
     testSatellite.avgAlt = 160;
     testSatellite.setSatilleteDangerStatus();
     expect(testSatellite.orbitalEmergencyStatus).toBe("Altitude is A-OK")
 })
 
 test("<160 Altitude Returns Warning Rapid Decay Status",()=>{
+    testSatellite.emergencyCountDown = 60;
     testSatellite.avgAlt = 159;
     testSatellite.setSatilleteDangerStatus();
     expect(testSatellite.orbitalEmergencyStatus).toBe("WARNING: RAPID ORBITAL DECAY IMMINENT")
 })
 
 test("Properly returns status as sustained low orbit after altitude>=160 ",()=>{
+    testSatellite.emergencyCountDown = 60;
     testSatellite.avgAlt = 159;
     testSatellite.setSatilleteDangerStatus();
     testSatellite.avgAlt = 160;
@@ -25,6 +28,7 @@ test("Properly returns status as sustained low orbit after altitude>=160 ",()=>{
 })
 
 test("Properly returns status as A-OK after altitude>=160 for 20 runs (1 minute) ",()=>{
+    testSatellite.emergencyCountDown = 60;
     testSatellite.avgAlt = 159;
     testSatellite.setSatilleteDangerStatus();
     testSatellite.avgAlt = 160;
